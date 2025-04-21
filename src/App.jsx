f0f4befb9d0ec444c4d4f1e06e9f3f3ee16eda09
shivam69x <159170281+shivam69x@users.noreply.github.com>
2025-04-21 06:44:52 +0530

    Add Weather.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import bgw_image from "./assets/bgw.jpg";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <Weather/>
+  );
+}
+
+export default App;
+
+
+
+// "placeholder:text-sm text-lg text-white-800 rounded-md p-2 font-semibold bg-transparent transition duration-800 ease focus:outline-none focus:border-slate-400 hover:border-slate-700 focus:shadow focus:border-2  shadow-xl"
+
+
+
+
+
+
+
+// Button code
+
+// <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+//   Shimmer
+// </button>
+
+// tailwind.config.js code
+
+// "bg-transparent border-2 px-8 bg-opacity-40 rounded-md p-2 grid place-items-center"
